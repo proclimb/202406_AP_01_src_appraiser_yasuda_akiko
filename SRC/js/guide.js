65ecebf7b0c5572fa7ad2c126d1df36608bef0da
yasudaakiko <202406ap01_yasuda@x-ide.sakura.ne.jp>
2024-09-06 16:08:03 +0900

    物件検索で「＞」や「＜」の押下時の、データの移動、表示、背景色を修正

--- SRC/js/guide.js
+++ SRC/js/guide.js
@@ -66,7 +66,7 @@ function fnGuideMove(muki, articleNo) {
 		// 名称の取得
 		var articleName = $(obj.children()[0]).text();
 		var articleRoom = $(obj.children()[1]).text();
-		var articleKey = $(obj.children()[2]).text();
+		var articleKey = $(obj.children()[2]).html();
 
 		// 登録リストに保存(右側に移動)
 		tags = "<tr id=\"r" + articleNo + "\">";
@@ -102,17 +102,16 @@ function fnGuideMove(muki, articleNo) {
 		tags = "<tr id=\"s" + articleNo + "\">";
 
 		// 物件名の作成
-		tags += "<th>" + $(obj.children()[1]).text() + "</th>";
+		tags += "<td>" + $(obj.children()[1]).html() + "</td>";
 
 		// 部屋番号の作成
-		tags += "<td>" + $(obj.children()[2]).text() + "</td>";
 		tags += "<td>" + $(obj.children()[2]).html() + "</td>";
 
 		// 鍵情報の作成
 		tags += "<td>" + $(obj.children()[3]).html() + "</td>";
 
 		// 登録ボタンの作成
-		tags += "<td><input type=\"button\" value=\"&gt;\" /></td>";
+		tags += "<td><input type=\"button\" value=\"&gt;\" onClick=\"fnGuideMove( 'toRight', '" + articleNo + "' );\" /></td>";
 		tags += "</tr>";
 
 		// 検索側に追加
@@ -123,10 +122,10 @@ function fnGuideMove(muki, articleNo) {
 	}
 
 	// 色の再設定
-	$("#search tr").filter(":odd").find("td").attr("class", "list_td1");
+	$("#search tr").filter(":odd").find("td").attr("class", "list_td0");
 	$("#search tr").filter(":even").find("td").attr("class", "list_td1");
-	$("#regist tr").filter("odd").find(".td").attr("class", "list_td0");
-	$("#regist tr").filter("even").find(".td").attr("class", "list_td1");
+	$("#regist tr").filter(":odd").find("td").attr("class", "list_td0");
+	$("#regist tr").filter(":even").find("td").attr("class", "list_td1");
 }
 
 
