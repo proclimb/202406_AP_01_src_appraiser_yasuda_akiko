1e962c72eb65f05537bd9c943779a8ea17c86a70
yasudaakiko <202406ap01_yasuda@x-ide.sakura.ne.jp>
2024-09-18 14:14:50 +0900

    ユーザー情報画面や更新画面のパスワードは非表示

--- SRC/libAdminUser.php
+++ SRC/libAdminUser.php
@@ -41,7 +41,7 @@ function subAdminUser()
 					<tr>
 						<td class="list_td<?php print $i; ?>"><a href="javascript:form.act.value='adminUserEdit';form.userNo.value=<?php print $userNo; ?>;form.submit();"><?php print $name; ?></a></td>
 						<td class="list_td<?php print $i; ?>"><?php print $id; ?></td>
-						<td class="list_td<?php print $i; ?>"><?php print $password; ?></td>
+						<td class="list_td<?php print $i; ?>"></td>
 						<td class="list_td<?php print $i; ?>"><?php print fnAuthorityName($authority); ?></td>
 						<td class="list_td<?php print $i; ?>">
 							<?php if ($userNo > 1) { ?>
@@ -109,7 +109,7 @@ function subAdminUserEdit()
 			</tr>
 			<tr>
 				<th>PASS<span class="red">（必須）</span></th>
-				<td><input type="text" name="password" value="<?php print $password; ?>" /></td>
+				<td><input type="text" name="password" value="" /></td>
 			</tr>
 			<tr>
 				<th>所属</th>
